c95d39dfe4c90720cb93161c58c01b5f8b60f6e9
= <josegustavocoelho@gmail.com>
2020-04-21 12:57:07 -0300

    On branch master  Your branch is up to date with 'github/master'.
     Changes to be committed:
    	new file:   README.md
    	new file:   __pycache__/markov.cpython-37.pyc
    	new file:   __pycache__/reddit_config.cpython-37.pyc
    	new file:   autocomplete_bot.py
    	new file:   config_reddit.py
    	new file:   examples.txt
    	new file:   markov.py
    	new file:   speech sample 2.txt
    	new file:   speech sample 3.txt
    	new file:   speech sample 4.txt
    	new file:   speech sample.txt

---
+++ config_reddit.py
@@ -0,0 +1,6 @@
+#Data for reddit
+user = YOUR ACCOUNT HERE
+password = YOUR PASSWORD HERE
+client_id = "LJU-zUmur6WjLA"
+client_secret = "VsnVr8Sskq9FZRmlTjhjTSCTyZ4"
+database = "markov_database.db"
